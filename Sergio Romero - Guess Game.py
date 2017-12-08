d6c6cbb3451133d0476af634e24d78404c9fe7ad
SergioRmero <33761407+SergioRmero@users.noreply.github.com>
2017-12-08 10:40:54 -0800

    12.8.17
    Guess Game

--- Sergio Romero - Guess Game.py
+++ Sergio Romero - Guess Game.py
@@ -4,9 +4,20 @@ import random
 # does the guess match the number?
 # add "higher" and "lower"
 # add 5 guesses
+number = random.randint(0,50)
 
-number = random.randint
+guesses_used = 0
+
+if guesses_used != 5:
+    print("Type in a number between 0 and 50")
+while True:
+    guess = int(input("Guess: "))
+    if guess > number:
+        print("Try Again, Guess Lower!")
+    elif guess < number:
+        print("Try Again, Guess Higher!")
+    elif guess == number:
+        break
 
 
-
-
+print("Good Job!")
--- Warmups.py
+++ Warmups.py
@@ -1,31 +1,39 @@
 # 12.4.17
-def reverse_order(first_name, last_name):
-    print ("%s, %s" % (last_name, first_name))
+#def reverse_order(first_name, last_name):
+   # print ("%s, %s" % (last_name, first_name))
     # or print (last_name + " " + first_name
 
 
-reverse_order("Sergio", "Romero")
+#reverse_order("Sergio", "Romero")
 
 
 # 12.5.17
-def add_py(name):
-    print("%s.py" % name)
+#def add_py(name):
+ #   print("%s.py" % name)
     # or print(name + .py)
 
 
-add_py("Sergio") == "Sergio.py"
+#add_py("Sergio") == "Sergio.py"
 
 
 # 12.6.17
-def add(num1, num2, num3): # can call parameters whatever you want
-    print(num1 + num2 + num3)
+#def add(num1, num2, num3): # can call parameters whatever you want
+  #  print(num1 + num2 + num3)
 
 
-add(15, 18, 9000)
-add(60, 90, 100)
+#add(15, 18, 9000)
+#add(60, 90, 100)
 
 
 # 12.7.17
-for x in range(3):
-    print("Hello")
+#for x in range(3):
+  #  print("Hello")
 
+
+# 12.8.17
+def date(month, day, year):
+    #print(month + "/" + day + "/" + year)
+    print("%s/%s/%s" % (month, day, year))
+
+
+date(12, 8, 17)
