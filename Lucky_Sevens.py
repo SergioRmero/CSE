d696e2b7bd0bd8caef71b8b454d089fdca5aafa8
SergioRmero <33761407+SergioRmero@users.noreply.github.com>
2018-01-11 09:45:00 -0800

    Lucky Sevens
    Started Lucky Sevens

---
+++ Lucky_Sevens.py
@@ -0,0 +1,14 @@
+import random
+
+D1 = random.randint(1, 6)
+D2 = random.randint(1, 6)
+money = 15
+
+print("Lets play Lucky Sevens!")
+print("Bet one dollar to get started!")
+print("Hint: type $1")
+one_dollar = input(">_")
+
+while D1 + D2 == 7:
+    
+
