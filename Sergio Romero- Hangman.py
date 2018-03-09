16f337a7818ba3ead1aee4ee63b9f4109088ee3c
SergioRmero <33761407+SergioRmero@users.noreply.github.com>
2018-03-08 20:04:37 -0800

    Update Sergio Romero- Hangman.py

--- Sergio Romero- Hangman.py
+++ Sergio Romero- Hangman.py
@@ -1,5 +1,8 @@
+import random
+
 word_bank = ['name', 'science', 'election', 'overeat', 'dramatic', 'revoke', 'positive', 'tide', 'knuckles', 'victory']
-shown_word = ['_'] * len(word_bank)
+chosen = random.choice(word_bank)
+shown_word = '*' * len(chosen)
 Input = ">_"
 lives = 10
 letter_number = 0
@@ -10,18 +13,18 @@ while lives != 0:
     print("You have " + str(lives) + " lives left")
     Input = raw_input("Enter guess: ")
 
-    while letter_number < len(word_bank):
-        if word_bank[letter_number] == Input:
-            shown_word[letter_number] = Input
+    while letter_number < len(chosen):
+        if chosen[letter_number] == chosen:
+            list(shown_word)[letter_number] = chosen
             correct_guesses += 1
         letter_number += 1
 
     lives -= 1
 
-    if correct_guesses == len(word_bank):
+    if correct_guesses == len(chosen):
         print("You won!")
         break
 
     if lives == 0:
         print("You lost")
-        print()
+        print("The word was " + chosen)
