c0da1d7102cc9a195764fc32dc5cdbaf8d941a62
SergioRmero <33761407+SergioRmero@users.noreply.github.com>
2018-03-09 10:37:35 -0800

    Update Sergio Romero- Hangman.py

--- Sergio Romero- Hangman.py
+++ Sergio Romero- Hangman.py
@@ -2,29 +2,27 @@ import random
 
 word_bank = ['name', 'science', 'election', 'overeat', 'dramatic', 'revoke', 'positive', 'tide', 'knuckles', 'victory']
 chosen = random.choice(word_bank)
-shown_word = '*' * len(chosen)
-Input = ">_"
 lives = 10
-letter_number = 0
-correct_guesses = 0
-
+word_list = list(chosen)
+print(word_list)
+shown_word = []
+for letter in chosen:
+    shown_word.append("*")
 while lives != 0:
-    print("The word is:" + str(shown_word))
+    temporary_list = []
+    print("The word is: " + ''.join(shown_word))
     print("You have " + str(lives) + " lives left")
-    Input = raw_input("Enter guess: ")
-
-    while letter_number < len(chosen):
-        if chosen[letter_number] == chosen:
-            list(shown_word)[letter_number] = chosen
-            correct_guesses += 1
-        letter_number += 1
-
-    lives -= 1
-
-    if correct_guesses == len(chosen):
-        print("You won!")
-        break
+    Input = input("Enter guess: ")
+    for letter1, letter2 in zip(word_list, shown_word):
+        if Input == letter1:
+            temporary_list.append(Input)
+        elif letter2 != "*":
+            temporary_list.append(letter2)
+        else:
+            temporary_list.append("*")
+    shown_word = temporary_list
 
     if lives == 0:
         print("You lost")
         print("The word was " + chosen)
+        break
